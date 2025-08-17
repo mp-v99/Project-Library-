17d9f5617f5eca9f6ed2e602945a63396b85f298
mpv <193361506+mp-v99@users.noreply.github.com>
2025-08-16 21:03:31 -0600

    Add function:
    Define the displayBook function which handles
    the DOM behavior to display the array items.
    
    This isn't the final version as it has un-scalable
    features like displaying only the first item of
    the array when invoked.
    
    Therefore, this commit purpose is to leave record
    of the DOM events behavior, creating a div and
    appending that div into the DOM.

--- script.js
+++ script.js
@@ -2,6 +2,10 @@
 
 const myLibrary = [];
 
+// The book cards content:
+
+const libraryMainContent = document.querySelector('.main_content');
+
 // The form buttons to create new books:
 
 const bookInputTitle = document.querySelector('#title');
@@ -40,10 +44,40 @@ const createBook = function(title, author, pages, toggleRead) {
     myLibrary.push(newBook);
 }
 
-// The one event needed to grab the input 
+const displayBook = function(newBook) {
+    const newBookDiv = document.createElement('div');
+    newBookDiv.className = "card";
+    newBookDiv.textContent = newBook;
+    libraryMainContent.appendChild(newBookDiv);
+
+    // newTitleDiv = document.createElement('div');
+    // newTitleDiv.className = "";
+}
+
+// The one event needed to grab the input values 
 
 bookSubmitButton.addEventListener('click', (e) => {
     e.preventDefault();
     createBook(bookInputTitle.value, bookInputAuthor.value, bookInputPages.value, bookInputRead.checked);
-    console.log(myLibrary[0].bookInfo())
+    displayBook(myLibrary[0].title)
 })
+
+
+/* <div class="card">
+<div class="title">
+    <p>The Three-Body Problem<p>
+</div>
+<div class="author">
+    <p>By: Liu Cixin (刘慈欣)</p>
+</div>
+<div class="pages">
+    <p>416 pages</p>
+</div>
+<div class="read_toggle">
+    <input type="checkbox" class="tgl tgl-flip" id="switch_yes_no" checked/>
+    <label for="switch_yes_no" class="tgl_btn" data-tg-on="Read ✔" data-tg-off="Reading..."></label>
+</div>
+<div class="delete_button">
+    <button type="button" id="delete_btn">✖</button>
+</div>
+</div> */
