d34fa1b6236025c21a1bd3db5130dc72df9ec60f
mpv <193361506+mp-v99@users.noreply.github.com>
2025-08-17 00:44:09 -0600

    Finish Function & Fix bug:
    Finish adding the content for displayBook()
    function, which was defined in previous commit.
    
    On this commit, I added the rest of the content
    besides title.
    
    Bugfix:
    
    In previous commits, there was an issue with cards
    toggle checkbox only working for the original card
    
    This is because all of the inputs shared the exact
    same id.
    
    On this commit, we add the counter variable which
    helps create a unique id for each input.
    
    Another option could have been to use:
    
        crypto.randomUUID();
    
    It was avoided to avoid making the DOM to lengthy

--- script.js
+++ script.js
@@ -15,6 +15,10 @@ const bookInputRead = document.querySelector('#read_status');
 
 const bookSubmitButton = document.querySelector('#submit_button');
 
+// The toggle inputs counter:
+
+let counter = 0;
+
 // The Book function constructor:
 
 function Book(title, author, pages, toggleRead) {
@@ -41,17 +45,76 @@ function Book(title, author, pages, toggleRead) {
 const createBook = function(title, author, pages, toggleRead) { 
     const newBook = new Book(title, author, pages, toggleRead);
 
-    myLibrary.push(newBook);
+    myLibrary.unshift(newBook);
 }
 
 const displayBook = function(newBook) {
+    // Card Div
     const newBookDiv = document.createElement('div');
     newBookDiv.className = "card";
-    newBookDiv.textContent = newBook;
-    libraryMainContent.appendChild(newBookDiv);
 
-    // newTitleDiv = document.createElement('div');
-    // newTitleDiv.className = "";
+    // Title Div
+
+    const newTitleDiv = document.createElement('div');
+    newTitleDiv.className = "title";
+    newTitleDiv.textContent = newBook.title;
+   
+
+    // Author Div
+
+    const newAuthorDiv = document.createElement('div');
+    newAuthorDiv.className = "author";
+    newAuthorDiv.textContent = `By: ${newBook.author}`;
+
+    // Number of Pages Div
+
+    const newPagesDiv = document.createElement('div');
+    newPagesDiv.className = "pages";
+    newPagesDiv.textContent = `${newBook.pages} pages`;
+
+    // Read Toggle Div
+
+    counter++;
+
+    const newToggleDiv = document.createElement('div');
+    newToggleDiv.className = "read_toggle";
+
+    const inputToggle = document.createElement('input');
+    inputToggle.type = "checkbox";
+    inputToggle.className = "tgl tgl-flip";
+    inputToggle.id = `switch_yes_no${counter}`;
+    inputToggle.checked = newBook.toggleRead;
+
+    const labelToggle = document.createElement('label');
+    labelToggle.className = "tgl_btn";
+    labelToggle.setAttribute('for', inputToggle.id);
+    labelToggle.setAttribute('data-tg-on', "Read");
+    labelToggle.setAttribute('data-tg-off', "Reading...");
+
+    // Delete Button Div
+
+    const newDeleteDiv = document.createElement('div');
+    newDeleteDiv.className = "delete_button";
+
+    const newDeleteButton = document.createElement('button');
+    newDeleteButton.type = "button";
+    newDeleteButton.className = "delete_btn";
+    newDeleteButton.textContent = "✖";
+
+    // Append Children:
+
+    libraryMainContent.prepend(newBookDiv);
+
+    newBookDiv.appendChild(newTitleDiv);
+    newBookDiv.appendChild(newAuthorDiv);
+    newBookDiv.appendChild(newPagesDiv);
+    newBookDiv.appendChild(newToggleDiv);
+    newBookDiv.appendChild(newDeleteDiv);
+
+    newToggleDiv.appendChild(inputToggle);
+    newToggleDiv.appendChild(labelToggle);
+
+    newDeleteDiv.appendChild(newDeleteButton);
 }
 
 // The one event needed to grab the input values 
@@ -59,7 +122,7 @@ const displayBook = function(newBook) {
 bookSubmitButton.addEventListener('click', (e) => {
     e.preventDefault();
     createBook(bookInputTitle.value, bookInputAuthor.value, bookInputPages.value, bookInputRead.checked);
-    displayBook(myLibrary[0].title)
+    displayBook(myLibrary[0])
 })
 
 
